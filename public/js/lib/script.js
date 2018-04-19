1d86043a6242327afb3a7bf0674c7cd78d90c601
Tatiana <tatiana@tatianasmacbook.local>
2018-04-19 18:09:41 +0300

    first commit

--- public/js/lib/script.js
+++ public/js/lib/script.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
   $('a').on('click', function(e) {
     e.preventDefault();
     var pageRef = $(this).attr('href');
-
     callPage(pageRef);
 
   });
@@ -15,6 +14,9 @@ $(document).ready(function() {
 
       success: function(response) {
         console.log('the page was loaded');
+        history.pushState('', '', ('#' + pageRefInput));
+        
+
         $("#main").css("display", "none");
         $('.content').html(response);
         if (pageRefInput === 'products_cake.html') {
